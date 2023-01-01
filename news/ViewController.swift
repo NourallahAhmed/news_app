35fb4b62741680ce4537d6640c9cea7544f4f257
NourallahAhmed <NourAllahahmed7001@gmail.com>
2023-01-01 07:59:49 -0800

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by NourAllah Ahmed on 01/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
